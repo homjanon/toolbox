f9698a3dacccf7a38cd4f0ac34b38975538c9cf1
homjanon <homjanon@gmail.com>
2026-09-19 10:35:42 +0800

    fix: ModuleActivity 补 import android.view.Window（上一提交编译失败）

--- android/app/src/main/java/com/homjanon/toolbox/ModuleActivity.java
+++ android/app/src/main/java/com/homjanon/toolbox/ModuleActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
